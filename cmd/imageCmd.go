7d7fa45547e571450acd6829552aba2f982151d4
Pradumna Saraf <pradumnasaraf@gmail.com>
2024-07-30 20:19:48 +0530

    Improve image and search commands

--- cmd/imageCmd.go
+++ cmd/imageCmd.go
@@ -17,11 +17,11 @@ var (
 )
 
 var imageCmd = &cobra.Command{
-	Use: "image [your question] --path [image path] --format [image format]",
+	Use:     "image [your question] --path [image path] --format [image format]",
 	Example: "gencli image 'What this image is about?' --path cat.png --format png",
-	Short: "Know details about an image (Please put your question in quotes)",
-	Long: "Ask a question about an image and get a response. You need to provide the path of the image and the format of the image. The supported formats are jpg, jpeg, png, and gif.",
-	Args:  cobra.MinimumNArgs(1),
+	Short:   "Know details about an image (Please put your question in quotes)",
+	Long:    "Ask a question about an image and get a response. You need to provide the path of the image and the format of the image. The supported formats are jpg, jpeg, png, and gif.",
+	Args:    cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		res := imageFunc(args)
 		fmt.Println(res)
