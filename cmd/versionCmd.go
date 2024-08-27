c5c314b50cd43cb2a85c2a8ff12649644df7b7a0
Pradumna Saraf <pradumnasaraf@gmail.com>
2024-08-27 23:59:33 +0530

    feat: add version and update command (#13)
    - feat: add version and update command
    - docs: Update new command in README
    - chore: Update CliVersion variable in the `cmd/version.go` file
    - ci: Enable create-summary option in releases workflow

---
+++ cmd/versionCmd.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const CliVersion = "v1.4.0"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Know the installed version of gencli",
+	Long:  `This command will help you to know the installed version of gencli`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("gencli version:", CliVersion)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
