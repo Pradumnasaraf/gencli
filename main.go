36e8d0d3a89c7e8e4b0608ce34da611b4394c12f
Pradumna Saraf <pradumnasaraf@gmail.com>
2025-08-30 12:15:34 +0530

    fix: update error message for missing `GOOGLE_API_KEY` environment variable (#33)
    - Enhanced the error message to include a direct link to the README for more guidance.
    - Removed the CheckAPIKey function as its logic is now integrated into the main function.

--- main.go
+++ main.go
@@ -12,11 +12,10 @@ func main() {
 	googleGeminiAPIKey := os.Getenv("GOOGLE_API_KEY")
 
 	if googleGeminiAPIKey == "" {
-		fmt.Println("Please set the GOOGLE_API_KEY environment variable. Check the README for more information.")
+		fmt.Println("Please set the GOOGLE_API_KEY environment variable. Check the https://github.com/Pradumnasaraf/gencli README for more information.")
 		return
 	}
 
 	cmd.SetDefaultConfig()
-	cmd.CheckAPIKey()
 	cmd.Execute()
 }
